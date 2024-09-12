d250c68fed1f1e95abcd904f50da10f27b68e812
Jose Rivera <joseariverala12@gmail.com>
2024-09-12 13:41:11 -0600

    Se agregaron comentarios al javascript

--- js/contactanos.js
+++ js/contactanos.js
@@ -1,23 +1,28 @@
+// Se obtienen los inputs del formulario
 let nombre_usuario = document.getElementById("nombre_usuario");
 let apellidos_usuario = document.getElementById("apellidos_usuario");
 let email_usuario = document.getElementById("email_usuario");
 let telefono_usuario = document.getElementById("telefono_usuario");
 let mensaje = document.getElementById("mensaje");
+// Se obtienen los div para mostrar los mensajes de error
 let nombresInfo = document.getElementById("nombresInfo");
 let apellidosInfo = document.getElementById("apellidosInfo");
 let emailInfo = document.getElementById("emailInfo");
 let telefonoInfo = document.getElementById("telefonoInfo");
 let mensajeInfo = document.getElementById("mensajeInfo");
+// Obtenemos el formulario
 let formulario = document.getElementById("formulario");
-let btnEnviar = document.getElementById("btnEnviar");
+// Inicializamos nuestra bandera isValid en tru
 let isValid = true
+// Declaramos las expresiones regulares para validar los datos
+const nombreRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/; // Expresión regular para el nombre y el apellido
+const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$/; // Expresión regular para el email
+const evitarCaracteres = /^[^'";<>\\\/&()\[\]]+$/ // Expresión regular para el mensaje
 
-const nombreRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
-const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$/;
-const evitarCaracteres = /^[^'";<>\\\/&()\[\]]+$/
-
+// Agregamos el evento cuando se envie el formulario
 formulario.addEventListener("submit",function (e) {
     e.preventDefault()
+    // Cada que entramos al evento dejamos un estado inicial
     isValid = true
     nombre_usuario.style.border = "";
     apellidos_usuario.style.border = "";
@@ -35,42 +40,33 @@ formulario.addEventListener("submit",function (e) {
     mensajeInfo.innerHTML="";
     mensajeInfo.display="none";
     
-    // console.log("que hongo")
+    // Comprobamos que el nombre proporcionado sea válido, si no es válido cambiamos el estado de nuestra bandera a falso
     if (!validate(nombre_usuario, nombresInfo)) {
         nombre_usuario.style.border = "solid red medium";
-       // console.log("error nombre")
         isValid = false
     }
+    // Comprobamos que el apellido proporcionado sea válido, si no es válido cambiamos el estado de nuestra bandera a falso
     if (!validate(apellidos_usuario, apellidosInfo)) {
         apellidos_usuario.style.border = "solid red medium";
-        //console.log("error apellido")
         isValid = false
     }
+    // Comprobamos que el email proporcionado sea válido, si no es válido cambiamos el estado de nuestra bandera a falso
     if (!validateEmail(email_usuario, emailInfo)) {
         email_usuario.style.border = "solid red medium";
-       // console.log("error email")
         isValid = false
     }
-
+    // Comprobamos que el número telefónico proporcionado sea válido, si no es válido cambiamos el estado de nuestra bandera a falso
     if (!validarCantidad()) {
         telefono_usuario.style.border = "solid medium red";
         isValid = false;
-       // console.log("error telefono")
     }
-
+    // Comprobamos que el mensaje proporcionado sea válido, si no es válido cambiamos el estado de nuestra bandera a falso
     if (!validateMensaje(mensaje)) {
         mensaje.style.border = "solid medium red";
         isValid = false;
-        //console.log("error mensaje")
     }
-
-    if (!isValid) {
-        Swal.fire({
-            icon: "error",
-            title: "Error en el llenado del formulario",
-            text: "Favor de corregir las casillas resaltadas en rojo",
-          });  
-    }else{
+    // Si se pasaron todas las validaciones, se enviará el formulario 
+    if (isValid) {
         const serviceID = 'default_service';
         const templateID = 'template_xtz5zgp';
         emailjs.sendForm(serviceID, templateID, this)
@@ -80,26 +76,33 @@ formulario.addEventListener("submit",function (e) {
             email_usuario.value = "";
             telefono_usuario.value = "";
             mensaje.value="";
-
+            // Si el correo se envio correctamente lanzará una alerta de que se envio correctamente
             Swal.fire({
                 icon: "success",
                 title: "El correo se envió correctamente!",
                 showConfirmButton: false,
                 timer: 1500
               });
-
+            // Si hubo algun error lanzará una alerta diciendo que lo intente más tarde junto con el error
         }, (err) => {
             Swal.fire({
                 icon: "error",
                 title: "Error al enviar el correo",
-                text: "Intenta enviar el correo más tarde",
+                text: "Intenta enviar el correo más tarde. \n Error: " + err ,
               });  
         });
+    // Si no pasó todas las validaciones mandará una alerta diciendo que corriga los datos ingresados
+    }else{
+        Swal.fire({
+            icon: "error",
+            title: "Error en el llenado del formulario",
+            text: "Favor de corregir las casillas resaltadas en rojo",
+          });  
     }
 })
 
+// Función que valida que los campos sean solo letras y que haya almenos 3 caracteres
 function validate(data, info) {
-
     if (data.value.length < 3) {
         info.innerHTML=`El campo debe de tener almenos 3 carácteres`;
         info.display="block";
@@ -113,12 +116,8 @@ function validate(data, info) {
     return true
 }
 
+// Función que valida que el correo tenga un formato válido
 function validateEmail(email, info) {
-    if (email.value.length < 3) {
-        info.innerHTML=`El correo debe de tener almenos 3 carácteres`;
-        info.display="block";
-        return false
-    }
     if (!emailRegex.test(email.value)) {
         info.innerHTML=`El correo debe de cumplir con el formato example@example.com`;
         info.display="block";
@@ -127,12 +126,8 @@ function validateEmail(email, info) {
     return true
 }
 
+// Función que valida que sea un número y que tenga de 10 a 13 caracteres 
 function validarCantidad() {
-    if (telefono_usuario.value.length == 0) {
-        telefonoInfo.innerHTML=`El campo telefono esta vacio`;
-        telefonoInfo.display="block";
-        return false;
-    }// legth==0
     if (isNaN(telefono_usuario.value)) {
         telefonoInfo.innerHTML=`El campo telefono solo puede contener números`;
         telefonoInfo.display="block";
@@ -146,7 +141,7 @@ function validarCantidad() {
 
     return true;
 }
-
+// Función que valida que el mensaje tenga almenos 3 caracteres y que no contenga ciertos caracteres especiales 
 function validateMensaje(mensaje) {
     if (mensaje.value.length < 3) {
         mensajeInfo.innerHTML=`El mensaje debe de contener al menos 3 carácteres`;
@@ -159,7 +154,6 @@ function validateMensaje(mensaje) {
         return false
     }
     return true
-
 }
 
 
