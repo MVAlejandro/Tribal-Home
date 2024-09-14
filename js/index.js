8685d4a362da5a3748941964d99031da2f82c6c1
Jose Rivera <joseariverala12@gmail.com>
2024-09-14 00:22:50 -0600

    Se modiificó la barra de navegación

--- js/index.js
+++ js/index.js
@@ -1,66 +1,77 @@
 const encabezado= document.getElementById("encabezado")
 const pie=document.getElementById("pie")
 
-
 window.addEventListener("load",
     function(event){
         encabezado.insertAdjacentHTML("afterbegin",
-        `<nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand col-3" href="../index.html"><img src="./assets/image 5 1.png" alt="Logo_Tribal-Home" class="logo"></a>
-            <div class="collapse navbar-collapse col-sm-5" id="navbarNavDropdown">
-              <ul class="navbar-nav mx-auto">
-                  <li class="nav-item ms-4 me-4">
-                      <a class="nav-link" aria-current="page" href="../productos.html">Productos</a>
-                  </li>
-                  <li class="nav-item ms-4 me-4">
-                      <a class="nav-link" href="servicios.html">Servicios</a>
-                  </li>
-                  <li class="nav-item ms-4 me-4">
-                      <a class="nav-link" href="contactanos.html">Contacto</a>
-                  </li>
-                  <li class="nav-item ms-4 me-4">
-                      <a class="nav-link" href="nosotros.html">Nosotros</a>
-                  </li>
-              </ul>
-            </div>
-            <div class="col-4 text-end me-5">
-              <a class="text-dark col-1" href="../carrito.html"><img src="assets/bag-2.png" alt="carrito_de_compras" class="perfil-car" ></a>
-              <a class="text-dark col-1" href="../login.html"><img src="assets/user.png" alt="usuario" class="perfil-car"></a>
-              <button class="navbar-toggler col-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            </div>         
-        </div>
-    </nav>`)
+            `<nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="../index.html">
+                    <img src="assets/nav-bar/Logo.png" alt="Logo_Tribal-Home" class="logo">
+                    </a>
+                    <div class="d-lg-none ms-auto">
+                    <a class="text-dark me-2" href="../carrito.html">
+                        <img src="assets/nav-bar/bag-2.png" alt="carrito_de_compras" class="perfil-car">
+                    </a>
+                    <a class="text-dark me-2" href="../login.html">
+                        <img src="assets/nav-bar/user.png" alt="usuario" class="perfil-car">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    </div>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item ms-4 me-4">
+                            <a class="nav-link" href="../productos.html">Productos</a>
+                        </li>
+                        <li class="nav-item ms-4 me-4">
+                            <a class="nav-link" href="servicios.html">Servicios</a>
+                        </li>
+                        <li class="nav-item ms-4 me-4">
+                            <a class="nav-link" href="contactanos.html">Contacto</a>
+                        </li>
+                        <li class="nav-item ms-4 me-4">
+                            <a class="nav-link" href="nosotros.html">Nosotros</a>
+                        </li>
+                    </ul>
+                    </div>
+                    <div class="d-none d-lg-flex align-items-center ms-auto">
+                        <a class="text-dark nav-link me-3" href="../carrito.html">
+                            <img src="assets/nav-bar/bag-2.png" alt="carrito_de_compras" class="perfil-car">
+                        </a>
+                        <a class="text-dark nav-link me-3" href="../login.html">
+                            <img src="assets/nav-bar/user.png" alt="usuario" class="perfil-car">
+                        </a>
+                    </div>  
+                </div>
+            </nav>`
+        );
 
-    pie.insertAdjacentHTML("beforeend",
-    `<div class="container-fluid">
-    <hr>
-    <div class="row align-items-center">
-        <div class="col text-start">
-            <p class="mb-0">Tribal-Home © Copyright 2024, Inc. All rights reserved</p>
-         </div>
-        <div class="col text-end">
-            <a href="#" class="text-dark me-3"><img src="assets/Path.png" alt="Facebook" class="footer-icon"></a>
-            <a href="#" class="text-dark me-3"><img src="assets/Path (1).png" alt="Twitter" class="footer-icon"></a>
-            <a href="#" class="text-dark"><img src="assets/Shape.png" alt="Instagram"  class="footer-icon"></a>
-        </div>
-    </div>
-</div>`
-)
+        pie.insertAdjacentHTML("beforeend",
+            `<div class="container-fluid">
+                <hr>
+                <div class="row align-items-center">
+                    <div class="col text-start">
+                        <p class="mb-0">Tribal-Home © Copyright 2024, Inc. All rights reserved</p>
+                    </div>
+                    <div class="col text-end">
+                        <a href="#" class="text-dark me-3"><img src="assets/footer/Facebook-icon.png" alt="Facebook" class="footer-icon"></a>
+                        <a href="#" class="text-dark me-3"><img src="assets/footer/Instagram-icon.png" alt="Twitter" class="footer-icon"></a>
+                        <a href="#" class="text-dark"><img src="assets/footer/Twitter-icon.png" alt="Instagram"  class="footer-icon"></a>
+                    </div>
+                </div>
+            </div>`
+        );
+
+        const currentLocation = window.location.href;
+        const menuItems = document.querySelectorAll('.nav-link');
+        
+        menuItems.forEach(item => {
+            if (item.href === currentLocation) {
+                item.classList.add('active');
+            }
+        });
 
     }
 )
-
-
-
-
-
-
-
-
-
-
-
-
