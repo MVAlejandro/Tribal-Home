2a1aa3f208d29210c8f309ba6fcedcdabb5ba583
Padeon <padeon22@gmail.com>
2024-09-24 10:48:35 -0600

    Se añadieron los la instancia de los elementos en los archivos js y se corrigio el estilo de la pagina login.html

---
+++ js/login.js
@@ -0,0 +1,17 @@
+
+//botones
+const btn_facebook= document.getElementById("btn-facebook");
+const btn_google= document.getElementById("btn-google");
+const btn_cuestionario= document.getElementById("btn_cuestionario")
+//campos
+const name_field= document.getElementById("name-field");
+const email_field= document.getElementById("email-field");
+const password_field= document.getElementById("password-field");
+//variables
+let isValid=false 
+
+//debe validar la existencia del usuario y mandar a la pagina de inicio
+//puede usar url o la sesion storage para mandar informacion del usuario
+btn_cuestionario.addEventListener("click", function() {
+    
+})
--- js/registro.js
+++ js/registro.js
@@ -1,3 +1,21 @@
+//botones
+const btn_cuestionario=document.getElementById("btn_cuestionario");
+//campos-input
+const nombre_usuario=document.getElementById("nombre_usuario");
+const apellidos_usuario=document.getElementById("apellidos_usuario");
+const cp_usuario=document.getElementById("cp_usuarioo");
+const direccion_usuario=document.getElementById("direccion_usuarioo");
+const email_usuario=document.getElementById("email_usuario");
+const telefono_usuario=document.getElementById("telefono_usuario");
+
+//campos-info
+const nombresInfo=document.getElementById("nombresInfo");
+const apellidosInfo=document.getElementById("apellidosInfo");
+const cpInfo=document.getElementById("cpInfo");
+const direccionInfo=document.getElementById("direccionInfo");
+const emailInfo=document.getElementById("emailInfo");
+const telefonoInfo=document.getElementById("telefonoInfo");                         
+                              
 //declara el div en done se pondra el select de estados
 const ubicacion_usuarios= document.getElementById("ubicacion_usuarios");
 //declara arreglos de estado
@@ -5,6 +23,7 @@ const arrEstados=["Aguascalientes", "Baja California", "Baja California Sur", "C
    " Chiapas", "Chihuahua", "Durango", "Distrito Federal"," Guanajuato", "Guerrero","Hidalgo", "Jalisco", "México", "Michoacán", 
   " Morelos", "Nayarit","Nuevo León", "Oaxaca", "Puebla", "Querétaro", "Quintana Roo", "San Luis Potosí", "Sinaloa", "Sonora", "Tabasco", 
   "Tamaulipas", "Tlaxcala", "Veracruz", "Yucatán", "Zacatecas"];
+//*--------------funciones-----------------*
 //llena el select con el arreglo de los estados
 function llenarSelect(){
     let cad=""
@@ -17,6 +36,13 @@ function llenarSelect(){
 
 }
 
+//debe de verificar los datos y de ser correcto crear la cuenta por el momento en el local storage
+btn_cuestionario.addEventListener("click", function () {
+    
+})
+
+
+//*---------------metodo al cargar pagina-----------*
 window.addEventListener("load", function(){
     llenarSelect();
 });
