05c9bc337fb8bd47bb6fb06af5f0237141982eaa
Jose Rivera <joseariverala12@gmail.com>
2024-09-27 20:08:15 -0600

    Se agregó la funcionalidad de la visualización del perfil

---
+++ js/perfil.js
@@ -0,0 +1,17 @@
+// Se agregan campos
+const data = document.getElementById("data");
+
+window.addEventListener("load", function(event){
+    event.preventDefault();
+    if(this.localStorage.getItem("usuario_activo") == null){
+        location.href = "./index.html";
+    }else{
+        let usuario = JSON.parse(this.localStorage.getItem("usuario_activo"))
+        data.insertAdjacentHTML("afterbegin", `
+                <p>${usuario.nombre_usuario}  ${usuario.apellidos_usuario}</p>
+                <p>${usuario.direccion_usuario}</p>
+                <p>${usuario.telefono_usuario}</p>
+                <p>${usuario.email_usuario}</p>
+            `)
+    }
+})
